bcdda4424234a3414279ab85b4da90e3864b220e
Ishaan Mantripragada <60798290+28ishaan@users.noreply.github.com>
2024-05-23 21:45:25 -0700

    Update resnet_enlarged.py

--- models/resnet_enlarged.py
+++ models/resnet_enlarged.py
@@ -174,7 +174,7 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 # Save the predictions DataFrame to a CSV file
-output_file_path = os.path.join(output_dir, 'predictions_resnet152_cardio_3epoch.csv')
+output_file_path = os.path.join(output_dir, 'predictions_resnet152_enlarged_3epoch.csv')
 df_output.to_csv(output_file_path, index=False)
 
 print(f"DataFrame saved to {output_file_path}")
