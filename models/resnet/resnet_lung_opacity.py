849d5e6fe2c16985e51aedfb846048e30c3b5e9c
Ishaan Mantripragada <60798290+28ishaan@users.noreply.github.com>
2024-05-23 21:53:10 -0700

    Create resnet_lung_opacity.py

---
+++ models/resnet/resnet_lung_opacity.py
@@ -0,0 +1,2 @@
+labels_of_interest = ['No Finding', 'Enlarged Cardiomediastinum', 'Cardiomegaly', 'Lung Opacity', 'Pneumonia', 
+                      'Pleural Effusion', 'Pleural Other', 'Fracture', 'Support Devices']
