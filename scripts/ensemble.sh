695cef6d2faf9e303fe742d5d097f74236032c68
28ishaan <60798290+28ishaan@users.noreply.github.com>
2024-05-28 20:45:12 -0700

    ensemble.sh script

---
+++ scripts/ensemble.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=ensemble
+#SBATCH -A CS156b
+#SBATCH --time=16:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --gres gpu:3
+#SBATCH --mail-user=imantrip@caltech.edu
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --partition=gpu
+#SBATCH --mem=64G
+#SBATCH --output=/groups/CS156b/2024/butters/%j.out
+#SBATCH --error=/groups/CS156b/2024/butters/%j.err
+
+source /home/ecasanov/CS156b/myenv/bin/activate
+cd /groups/CS156b/2024/butters
+python ensemble.py
